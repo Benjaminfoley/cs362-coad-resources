bbfcd948a27acd636c7b285555c70621ed091e6f
berginp <berginp@oregonstate.edu>
2023-01-19 10:56:25 -0800

    test

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,7 @@
 # Purpose: To be the parent class of all models in the application. This is
 #          to ensure that all models are using the same connection to the
 #          database.
+#
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
