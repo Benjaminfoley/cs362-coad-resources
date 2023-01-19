583d14b6af41da5da2821c6a5405a6512eceb9f7
Seth Weiss <sweisss@gmail.com>
2023-01-19 15:34:57 -0800

    Deleted instantiation tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -3,10 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  it 'exists' do
-    Organization.new
-  end
-
   it 'has a name' do
     organization = Organization.new
     expect(organization).to respond_to(:name)
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -3,10 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  it 'exists' do
-    Ticket.new
-  end
-
   it 'has a name' do
     ticket = Ticket.new
     expect(ticket).to respond_to(:name)
