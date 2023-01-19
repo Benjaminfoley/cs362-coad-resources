534f6a7366efb18cdcbf688718e5e75acd59a1e4
Benjamin Foley <foleybe@oregonstate.edu>
2023-01-19 11:17:59 -0800

    Completed resource_category test

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+    
+      it "exists" do
+     ResourceCategory.new
+      end
+    
+      it "has a name" do
+     resource_category = ResourceCategory.new
+     expect(resource_category).to respond_to(:name)
+      end
+    
+      it "has a string representation that is its name" do
+     name = 'Camping'
+     resource_category = ResourceCategory.new(name: name)
+     result = resource_category.to_s
+      end
+    
 
 end
