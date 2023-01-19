8c57fd75c4fe25b7ed951b56209654d8cc48f8cf
Benjamin Foley <foleybe@oregonstate.edu>
2023-01-19 11:31:11 -0800

    completed ticket_spec.rb

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
+    
+      it "exists" do
+        Ticket.new
+      end
+    
+      it "has a name" do
+        ticket = Ticket.new
+        expect(ticket).to respond_to(:name)
+      end
+    
+      it "has a string representation that is its name" do
+        name = 'Mt. Hood'
+        ticket = Ticket.new(name: name)
+        result = ticket.to_s
+      end
+    
 
 end
