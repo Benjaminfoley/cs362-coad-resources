f6974aa13f49cba36f32cd5e1ac69b2dcc69e5fe
Benjamin Foley <foleybe@oregonstate.edu>
2023-01-19 11:10:31 -0800

    Wrote test for Organization_spec.rb

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+    it "exists" do
+        Organization.new
+    end
+    
+    it "has a name" do
+        organization = Organization.new
+        expect(organization).to respond_to(:name)
+    end
+    
+    it "has a string representation that is its name" do
+        name = 'Past Control'
+        organization = Organization.new(name: name)
+        result = organization.to_s
+    end
 end
