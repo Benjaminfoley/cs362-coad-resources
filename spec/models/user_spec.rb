4a024297980dbfee3e7108b9f26e6965bd56b1cb
berginp <berginp@oregonstate.edu>
2023-01-19 11:40:03 -0800

    user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+    it "exists" do
+        User.new
+      end
+    
+    it "has a email" do
+        user = User.new
+        expect(user).to respond_to(:email)
+      end
+    
+    it "has a string representation that is its email" do
+        email = 'test@domain.com'
+        user = User.new(email: email)
+        result = user.to_s
+      end
+
+
+
+    
+      
 end
