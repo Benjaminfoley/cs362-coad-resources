2bdc744a62aa88bc51cee17e0deb39c3d5ca711e
Seth Weiss <sweisss@gmail.com>
2023-01-19 16:40:03 -0800

    Added and passed 'has an active attribute' test.

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -17,12 +17,16 @@ RSpec.describe ResourceCategory, type: :model do
     expect(resource_category).to respond_to(:name)
   end
 
-  it 'has an validates_presence_of attribute' do
+  it 'has a validates_presence_of attribute' do
     expect(resource_category).to respond_to(:validates_presence_of)
   end
 
-  it 'has an validates_length_of attribute' do
+  it 'has a validates_length_of attribute' do
     expect(resource_category).to respond_to(:validates_length_of)
+  end
+
+  it 'has an active attribute' do
+    expect(resource_category).to respond_to(:active)
   end
 
   it 'has a string representation that is its name' do
