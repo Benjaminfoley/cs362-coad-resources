64031632431b8336c1fa776b07e5a15b47844901
Seth Weiss <sweisss@gmail.com>
2023-01-19 18:27:35 -0800

    Added and passed 'should validate the presence of primary_name' test.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -121,9 +121,9 @@ RSpec.describe Organization, type: :model do
     expect(organization).to validate_presence_of(:status)
   end
   
-  # it 'should validate the presence of primary_name' do
-  #   expect(organization).to validate_presence_of(:primary_name)
-  # end
+  it 'should validate the presence of primary_name' do
+    expect(organization).to validate_presence_of(:primary_name)
+  end
   
   # it 'should validate the presence of secondary_name' do
   #   expect(organization).to validate_presence_of(:secondary_name)
