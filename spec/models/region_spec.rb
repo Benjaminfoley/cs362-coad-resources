c658a8ab1d61671790c32172e8ddfccd61081cf2
Seth Weiss <sweisss@gmail.com>
2023-01-19 16:31:46 -0800

    Added and passed 'has a validates_presence_of attribute' test

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -13,7 +13,9 @@ RSpec.describe Region, type: :model do
     expect(region).to respond_to(:name)
   end
 
-
+  it 'has a validates_presence_of attribute' do
+    expect(region).to respond_to(:validates_presence_of)
+  end
 
   it 'has a string representation that is its name' do
     name = 'Mt. Hood'
