3203b580a8c595f16e0384be32b6da9e529bfe6f
Seth Weiss <sweisss@gmail.com>
2023-01-19 16:35:35 -0800

    Added a let to reduce redundancy

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -3,14 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+  let(:resource_category) {ResourceCategory.new}
+
   it 'has a name' do
-    resource_category = ResourceCategory.new
     expect(resource_category).to respond_to(:name)
   end
 
   it 'has a string representation that is its name' do
     name = 'Camping'
-    resource_category = ResourceCategory.new(name: name)
     result = resource_category.to_s
   end
 end
