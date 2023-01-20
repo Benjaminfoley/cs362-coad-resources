506568f25b7dd4b6d29a0c35e76da3616473ff65
Seth Weiss <sweisss@gmail.com>
2023-01-19 17:44:16 -0800

    Added and passed it 'has a role attribute' test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { User.new }
   
-  it 'has a email' do
+  it 'has an email attribute' do
     expect(user).to respond_to(:email)
+  end
+
+  it 'has a role attribute' do
+    expect(user).to respond_to(:role)
   end
 
   it 'has a string representation that is its email' do
