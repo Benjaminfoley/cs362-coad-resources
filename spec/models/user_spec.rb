068543b28bfce3901a11e8a1e2910eebf3212f08
Seth Weiss <sweisss@gmail.com>
2023-01-19 17:42:04 -0800

    Added let to reduce redundancy

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { User.new }
+  
   it 'has a email' do
-    user = User.new
     expect(user).to respond_to(:email)
   end
 
   it 'has a string representation that is its email' do
     email = 'test@domain.com'
-    user = User.new(email: email)
     result = user.to_s
   end
 end
