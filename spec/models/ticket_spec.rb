7ceeaac5825b907ead994c8e2f07d9b9cc1444cf
Seth Weiss <sweisss@gmail.com>
2023-01-19 17:36:43 -0800

    Added and passed 'has a phone attribute' test.

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -25,6 +25,10 @@ RSpec.describe Ticket, type: :model do
     expect(ticket).to respond_to(:name)
   end
 
+  it 'has a phone attribute' do
+    expect(ticket).to respond_to(:phone)
+  end
+
   it 'has a string representation that is its name' do
     name = 'Mt. Hood'
     result = ticket.to_s
