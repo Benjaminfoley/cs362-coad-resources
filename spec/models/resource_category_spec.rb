2b38edda8432fc5c1ab48a3c10ad5f8a744faf18
Benjamin Foley <foleybe@oregonstate.edu>
2023-01-24 22:11:46 -0800

    completed validation tests for resource_category

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -42,4 +42,16 @@ RSpec.describe ResourceCategory, type: :model do
     name = 'Camping'
     result = resource_category.to_s
   end
+  #3.1 Validations
+  describe 'should validate presence of name' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'should validate length of name' do
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(255) }
+  end
+  
+  describe 'should validate uniqueness of name' do
+    it { should validate_uniqueness_of(:name).case_insensitive }
+  end
 end
