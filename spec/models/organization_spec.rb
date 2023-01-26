d8d9ba7758e109b476ca5ec0bdf74722c42758fc
Benjamin Foley <foleybe@oregonstate.edu>
2023-01-26 09:22:18 -0800

    3.2 testing member function for to_s Organization_spec

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -151,4 +151,8 @@ RSpec.describe Organization, type: :model do
     name = 'Past Control'
     result = organization.to_s
   end
+  #3.2 Test member Functions
+  it "should respond" do
+    expect(organization).to respond_to(:to_s)
+  end
 end
