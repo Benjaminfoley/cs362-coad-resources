61ec6699383e1a3cce7c90821aa557843425c4d8
Seth Weiss <sweisss@gmail.com>
2023-02-14 11:15:42 -0800

    Added general testing structure comments to dashboard_controller_spec.rb

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
 
+    # Group 1: Admins
+        # test index
+
+        # test status options
+
+        # test tickets
+
+
+    # Group 2: Organization
+        # test index
+
+        # test status options
+
+        # test tickets
+
+
+    # Group 3: Normal User
+        # test index
+
+        # test status options
+
+        # test tickets
+        
+
 end
