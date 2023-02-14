2e4610cf8dbb05241c586474a542cc922f02a6a7
berginp <berginp@oregonstate.edu>
2023-02-14 11:43:26 -0800

    build factory

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe DashboardController, type: :controller do
 
     # Group 1: Admins
+    describe 'Admins' do
+      let(:signed_in_user) { build_stubbed(:user) }
+      before(:each) { sign_in signed_in_user }
+      
+    end
         # test index
 
         # test status options
