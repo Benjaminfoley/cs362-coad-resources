0140a2f4a9315374d36038e580ec4cefaee0ee1f
Seth Weiss <sweisss@gmail.com>
2023-02-16 14:22:04 -0800

    GET index controller test as admin in regions_controller_spec passed

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,26 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
-
-  # do we need to check for signed in?
-  # describe 'signed in?' do
-  #   it 'signed in' do
-  #     user = build_stubbed(:user)
-  #     expect(sign_in(user)).to be true
-  #   end
-  # end
-
-  # REMEMBER TO ONLY TEST IF SUCCESSFULL OR IF REDIRECT
+  # REMEMBER TO ONLY TEST IF SUCCESSFUL OR IF REDIRECT
   # login = new_user_session_path (redirect if you're not logged in)
   # admin = successful 
-  describe "GET #index" do
-    # let(:user) {build_stubbed(:user)}
-    let(:user) {create(:user)}
 
-    it {
-    sign_in(user)   # user not signing in?
-    expect(get(:index)).to redirect_to(dashboard_path) 
-  }
+  context 'as admin user' do
+    let(:admin) {create :user, :admin}
+    before(:each) { sign_in admin }
+    
+    describe 'GET #index' do
+      it 'is successful' do
+        expect(get(:index)).to have_http_status(:ok)
+      end
+    end
   end
 
+  # describe "GET #index" do
+  #   let(:user) {create :user}
+  #   let(:organization) {create :user, :organization}
+
+  #   # it 'redirects a non-logged-in user to sign-in' do
+  #   #   expect(get(:index)).to redirect_to(sign_in_path) 
+  #   # end
+
+  #   it 'redirects an organization user to dashboard' do
+  #     sign_in(organization)
+  #     expect(get(:index)).to redirect_to dashboard_path
+  #   end
+
+  #   it 'is successful for admin user' do
+  #     sign_in(admin)
+  #     expect(get(:index)).to be success
+  #   end
+  # end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,13 +1,9 @@
 FactoryBot.define do
     factory :user do
-      # name {"UserName"}
-      # email
       password { 'password' }
-      # password_confirmation { 'password' }
 
       # this creates a unique email each time the factory is called
       sequence(:email) { |n| "user_#{n}@email.com" }
-      # before(:build_stubbed) { |user| user.skip_confirmation! }
       before(:create) { |user| user.skip_confirmation! }
     end
 
