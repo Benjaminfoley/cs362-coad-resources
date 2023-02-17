52986a93da4b60b65c42ccb51f1425f487b4f32b
Seth Weiss <sweisss@gmail.com>
2023-02-16 23:18:48 -0800

    Skeleton structure added to resrouce_categories_spec.rb

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,5 +1,88 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
+  # REMEMBER TO ONLY TEST IF SUCCESSFUL OR IF REDIRECT
+  # login = new_user_session_path (redirect if you're not logged in)
+  # admin = successful
 
+  context 'as admin user' do
+
+    describe 'GET #index' do pending "Not yet implemented"
+    end
+
+    describe 'GET #show' do pending "Not yet implemented"
+    end
+
+    describe 'POST #create' do pending "Not yet implemented"
+    end
+
+    describe 'GET #edit' do pending "Not yet implemented"
+    end
+
+    describe 'PATCH #update' do pending "Not yet implemented"
+    end
+
+    describe 'PATCH #activate' do pending "Not yet implemented"
+    end
+
+    describe 'PATCH #deactivate' do pending "Not yet implemented"
+    end
+
+    describe 'DELETE #destroy' do pending "Not yet implemented"
+    end
+  end
+
+  context 'as organization' do
+
+    describe 'GET #index' do pending "Not yet implemented"
+    end
+
+    describe 'GET #show' do pending "Not yet implemented"
+    end
+
+    describe 'POST #create' do pending "Not yet implemented"
+    end
+
+    describe 'GET #edit' do pending "Not yet implemented"
+    end
+
+    describe 'PATCH #update' do pending "Not yet implemented"
+    end
+
+    describe 'PATCH #activate' do pending "Not yet implemented"
+    end
+
+    describe 'PATCH #deactivate' do pending "Not yet implemented"
+    end
+
+    describe 'DELETE #destroy' do pending "Not yet implemented"
+    end
+  end
+
+  context 'as a non-logged-in user' do
+
+    describe 'GET #index' do pending "Not yet implemented"
+    end
+
+    describe 'GET #show' do pending "Not yet implemented"
+    end
+
+    describe 'POST #create' do pending "Not yet implemented"
+    end
+
+    describe 'GET #edit' do pending "Not yet implemented"
+    end
+
+    describe 'PATCH #update' do pending "Not yet implemented"
+    end
+
+    describe 'PATCH #activate' do pending "Not yet implemented"
+    end
+
+    describe 'PATCH #deactivate' do pending "Not yet implemented"
+    end
+
+    describe 'DELETE #destroy' do pending "Not yet implemented"
+    end
+  end
 end
