61f7925167ba7a26300a47bca8860a62972931a4
Seth Weiss <sweisss@gmail.com>
2023-02-16 16:52:05 -0800

    POST #create for not-logged-in user in regions_controller_spec looks like it works

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -121,8 +121,11 @@ RSpec.describe RegionsController, type: :controller do
       end
     end
 
-    describe 'POST #create' do pending 
-      "Not yet implemented"
+    describe 'POST #create' do 
+      it 'is redirects to sign-in'do
+        post :create, params: { region: attributes_for(:region) }
+        expect(response).to redirect_to(user_session_path)
+      end
     end
 
     describe 'GET #edit' do pending 
