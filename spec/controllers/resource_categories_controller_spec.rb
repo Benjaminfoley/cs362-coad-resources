206e1a14b6b622cc6b12444feac1c637f0f869bc
berginp <berginp@oregonstate.edu>
2023-02-17 15:37:10 -0800

    up to patch #activate

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -7,24 +7,48 @@ RSpec.describe ResourceCategoriesController, type: :controller do
   # login = new_user_session_path (redirect if you're not logged in)
   # admin = successful
   context 'as admin user' do
+    let(:admin) { create :user, :admin }
+    let(:resource_category) { create :resource_category }
+
+    before(:each) { sign_in admin }
+
     describe 'GET #index' do
-      pending 'Not yet implemented'
+      it 'is successful' do
+        expect(get(:index)).to have_http_status(:ok)
+      end
     end
 
     describe 'GET #show' do
-      pending 'Not yet implemented'
+      it 'is successful' do
+        #get :show, params: { resource_category: nil } show method has no code
+        expect(response).to have_http_status(:ok)
+      end
     end
 
     describe 'POST #create' do
-      pending 'Not yet implemented'
+      it 'is redirects to resource_catagories_path path' do
+        post :create, params: { resource_category: attributes_for(:resource_category) }
+        expect(response).to redirect_to(resource_categories_path)
+      end
+
+      it 'renders :new when .save fails' do
+        post :create, params: { resource_category: { name: '' } }
+        expect(response).to render_template(:new)
+      end
     end
 
     describe 'GET #edit' do
-      pending 'Not yet implemented'
+      it 'is successful' do
+        get :edit, params: { id: resource_category.id }
+        expect(response).to have_http_status(:ok)
+      end
     end
 
     describe 'PATCH #update' do
-      pending 'Not yet implemented'
+      it 'is successful' do
+        patch :update, params: {id: resource_category.id, resource_category: attributes_for(:resource_category) }
+        expect(response).to have_http_status(:redirect)
+      end
     end
 
     describe 'PATCH #activate' do
