d08cc13e2f50c8f38f4bd810b0da4682c9c694ca
Seth Weiss <sweisss@gmail.com>
2023-02-16 23:50:45 -0800

    static_pages_congroller_spec.rb looks good. There really isn't anything in the controller code so it's hard to tell what we need to do.

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-
+  describe 'GET #index' do
+    it 'succeeds' do
+      expect(get(:index)).to have_http_status(:ok)
+    end
+  end
 end
