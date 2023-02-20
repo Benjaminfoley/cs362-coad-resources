0b87957b40c5ed96cb3eb598e135292b82f943c6
Benjamin Foley <foleybe@oregonstate.edu>
2023-02-19 16:35:55 -0800

    Passed as admin GET new

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe TicketsController, type: :controller do
   # admin = successful
   context 'as admin user' do
     describe 'GET #new' do
-      pending 'Not yet implemented'
+      it 'successful' do
+        expect(get(:new)).to have_http_status(:ok)
+      end
     end
 
     describe 'POST #create' do
