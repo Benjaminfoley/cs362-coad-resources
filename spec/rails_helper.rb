fd70d0d4a9f4475ddd89757551934147b90e9a90
Seth Weiss <sweisss@gmail.com>
2023-02-23 11:02:12 -0800

    Update SimpleCov.start in rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,9 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start 'rails' do
+  add_filter "app/helpers/"
+  add_filter "app/models/concerns/"
+  add_group 'Models', 'app/models'
+end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
