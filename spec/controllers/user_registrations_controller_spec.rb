118482f7d4428cd5896b7f86f011a2f737aabdf6
Seth Weiss <sweisss@gmail.com>
2023-02-23 14:48:30 -0800

    Add user_registrations_controller_spec

---
+++ spec/controllers/user_registrations_controller_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Users::RegistrationsController, type: :controller do
+  context 'as logged-out user' do
+    describe 'as logged-out user' do
+      it 'POST #create' do
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        post(:create, params: { user: attributes_for(:user) })
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+end
