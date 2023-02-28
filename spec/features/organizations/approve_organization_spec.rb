f05554516258e6685d11817195303598637b5bc2
Benjamin Foley <foleybe@oregonstate.edu>
2023-02-28 12:14:11 -0800

    Completed the test for features/approve organizations

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
-
+    let(:admin) { create(:user, :admin) }
+    before do
+        admin.confirm
+        log_in_as(admin)
+    end
+    it 'approves an organization' do
+        organization = create(:organization)
+        click_on 'Organizations'
+        click_on 'Pending'
+        click_on 'Review'
+        click_on 'Approve'
+        expect(page).to have_content('approved')
+    end
 end
