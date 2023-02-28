6b4347fe25626afd9ac7120584f372329dfb71f1
Benjamin Foley <foleybe@oregonstate.edu>
2023-02-28 13:21:45 -0800

    Passed admin Create Feature but didn't increase overall %

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
+     
+    let(:admin) { create(:user, :admin) }
+     
+    before do
+        admin.confirm
+        log_in_as(admin)
+    end
+     
+    it 'creates a region' do
+        click_on 'Regions'
+        click_on 'Add Region'
+      fill_in 'Name', with: 'Fake Region'
+      click_button 'Add Region'
+      expect(page).to have_content('Region successfully created.')
+    end
 
 end
