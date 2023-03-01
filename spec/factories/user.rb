ecba1e6000fc41a73ea4049328dc4d295d7d706a
berginp <berginp@oregonstate.edu>
2023-03-01 11:26:17 -0800

    create org application feature

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
     factory :user do
       password { 'password' }
+      organization_id { }
       # this sequence creates a unique email each time the factory is called
       sequence(:email) { |n| "user_#{n}@email.com" }
       before(:create) { |user| user.skip_confirmation! }
--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
-
+  scenario 'sign up' do
+    visit "/"
+    click_on 'Sign up'
+    fill_in 'Email', with: 'test@email.com'
+    fill_in 'Password', with: '1234567'
+    fill_in 'Password confirmation', with: '1234567'
+    click_button "Sign up"
+    expect(page).to have_content('A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.')
+  end
 end
