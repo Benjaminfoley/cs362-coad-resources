1272330bae519c181200a91f7261f7f4d6d6b426
berginp <berginp@oregonstate.edu>
2023-03-02 10:40:22 -0800

    update org spec

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
 
+  it 'updates an organization' do
+    user = create(:user, :admin)
+    log_in_as(user)
+    organization = create(:organization)
+    visit '/organizations'
+    click_on 'Pending'
+    click_on 'Review'
+    fill_in 'Rejection Reason', with: 'No'
+    click_on 'Reject'
+    expect(page).to have_content('rejected')
+    
+  end
+
 end
