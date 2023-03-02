a7605670364f7cd6d52c71b3ee5f261241a4d7dd
berginp <berginp@oregonstate.edu>
2023-03-02 11:51:09 -0800

    cant approve

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
-    let(:admin) { create(:user, :admin) }
-    before do
+    it 'approves an organization' do
+        admin = create(:user, :admin)
         admin.confirm
         log_in_as(admin)
-    end
-    it 'approves an organization' do
         organization = create(:organization)
         click_on 'Organizations'
         click_on 'Pending'
         click_on 'Review'
         click_on 'Approve'
         expect(page).to have_content('approved')
+    end
+
+    it 'Cant approve as organization' do
+        user = create(:user)
+        log_in_as(user)
+        within(page.find("#navbar")) do
+            expect(page).to_not have_content("Organizations")
+        end
     end
 end
