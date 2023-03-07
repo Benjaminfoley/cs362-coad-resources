cd2c638ae914f948f1c37a24fe8dbea298668353
Benjamin Foley <foleybe@oregonstate.edu>
2023-03-07 10:41:17 -0800

    Passed features/regions/admin_deletes_region_spec to delete a region

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
+    let(:admin) { create :user, :admin }
+    let(:region) { create :region }
+    
+    before(:each) { log_in_as admin }
+    
+    it 'is successful' do
+        visit region_path(region)
+        click_on 'Delete'
+        expect(page).to have_content("Region #{region} was deleted. Associated tickets now belong to the 'Unspecified' region.")
+    end
 
 end
