affd5ab59380467018f65c631ad09f188fc84346
Benjamin Foley <foleybe@oregonstate.edu>
2023-03-07 11:15:33 -0800

    I can't locate the correct capture/release button for cature ticket

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
+    #cannot find the CAPTURE button
+    # let(:user) { create :user }
+    # let(:ticket) { create :ticket }
+
+    # before(:each) { log_in_as user }
+
+    # it 'is successful' do
+    #     visit ticket_path(ticket)
+    #     # click_on 'Release'
+    #     click_link 'Capture'
+    #     # expect(page).to have_content("#{ticket} was captured.")
+    # end
 
 end
