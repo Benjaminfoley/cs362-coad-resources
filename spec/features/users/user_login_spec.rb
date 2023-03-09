b258703440d1e4ce992d2ce278866be3b4fc7220
berginp <berginp@oregonstate.edu>
2023-03-09 13:06:37 -0800

    user login user reg release ticket

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -2,14 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
   it'log in' do
-    visit '/signup'
-    fill_in "Email", with:"test@domain.com"
-    fill_in "Password", with:"password"
-    fill_in "Password confirmation", with:"password"
-    find('input[name="commit"][value="Sign up"]').click
+    user = create(:user)
     visit '/users/sign_in'
-    fill_in "Email", with:"test@domain.com"
-    fill_in "Password", with:"password"
+    fill_in "Email", with:"#{user.email}"
+    fill_in "Password", with:"#{user.password}"
     click_on "Sign in"
     save_and_open_page
     expect(page).to have_current_path("/dashboard")
