06bf69403dc0e36dd745a018f8e4b121aca04d40
berginp <berginp@oregonstate.edu>
2023-03-09 11:38:31 -0800

    capture and close

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -14,4 +14,27 @@ RSpec.describe 'Capturing a ticket', type: :feature do
     #     # expect(page).to have_content("#{ticket} was captured.")
     # end
 
+    it 'creates ticket then logs in to accept it' do
+        region = create(:region)
+        rc = create(:resource_category)
+        visit '/'
+        click_on 'Get Help'
+        fill_in 'Full Name', with: 'test'
+        fill_in 'Phone Number', with: '541-123-4565'
+        select("#{region.name}", from:"Region")
+
+        select("#{rc.name}",from:'Resource Category')
+        fill_in 'Description', with: 'test'
+        click_on 'Send this help request'
+        expect(page).to have_content("Ticket Submitted")
+        user = create(:user, :organization_approved)
+        visit '/'
+        log_in_as user
+        visit '/tickets/1'
+        click_on 'Capture'
+        expect(page).to have_current_path("/dashboard")
+
+
+    end
+
 end
