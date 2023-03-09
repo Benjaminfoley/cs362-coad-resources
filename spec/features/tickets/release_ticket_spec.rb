ebaaf4619e27de6747be46bd4bac953e6daa0bc3
berginp <berginp@oregonstate.edu>
2023-03-09 11:39:29 -0800

    release ticket

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'Releasing a ticket by an', type: :feature do
+  it 'creates ticket then logs in to accept it, then closed it' do
+    region = create(:region)
+    rc = create(:resource_category)
+    visit '/'
+    click_on 'Get Help'
+    fill_in 'Full Name', with: 'test'
+    fill_in 'Phone Number', with: '541-123-4565'
+    select("#{region.name}", from:"Region")
+
+    select("#{rc.name}",from:'Resource Category')
+    fill_in 'Description', with: 'test'
+    click_on 'Send this help request'
+    expect(page).to have_content("Ticket Submitted")
+    user = create(:user, :organization_approved)
+    visit '/'
+    log_in_as user
+    visit '/tickets/1'
+    click_on 'Capture'
+    visit '/tickets/1'
+    click_on 'Release'
+    expect(page).to have_current_path("/dashboard")
+
+
+
+end
+
 
 end
